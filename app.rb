f519251547f3fd89757156d088c380bfce3655c0
Kyle Smith <tman2kyle@hotmail.com>
2015-09-19 16:18:23 -0500

    circles after every growl no matter length size

--- app.rb
+++ app.rb
@@ -1,22 +1,40 @@
 require 'sinatra'
 require 'sinatra/reloader'
 enable :sessions
+# before do
 
+# end
 @@growl_array = []
-@@clickCounter = 0
 get '/' do
-    @@test_var = "HELLO GROWLER"
+    @@test_var = "test string   "
     erb :profile
 end
 
 post '/' do
-    @post = params[:post1]
-    @@growl_array.push(@post)
-    erb :profile
+#     greeting = params[:greeting] || "Hi There"
+#     name = params[:name] || "Nobody"
+
+@post = params[:post1]
+ 
+@@growl_array.push(@post)
+
+erb :profile
 end
 
 post '/reset' do
+
     @@growl_array=[]
     redirect '/'
 end
 
+get '/foo' do
+  session[:blair] = 'Hello blair'
+  person = "blair"
+  "other message"
+  # redirect to('/bar')
+end
+
+get '/bar' do
+
+  session[:blair]   # => 'Hello World!'
+end
